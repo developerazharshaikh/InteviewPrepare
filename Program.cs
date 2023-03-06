958cdd448fa72042fc218c39963fce3afdeae456
developerazharshaikh <developerazharshaikh@gmail.com>
2023-03-06 17:21:02 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InteviewPrepare.Main
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hi .Net Core");
+        }
+    }
+}
